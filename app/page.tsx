5498354b70f8271ba801231d37147fb5179c5e78
camelia <camelia.96str@gmail.com>
2025-11-24 13:34:43 +0100

    icons + ui visualizer first version.3

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { Progress } from "@/components/ui/progress";
-import { AddBook } from "@/components/ui/add-book-form";
-import { NativeSelect, NativeSelectOption } from "@/components/ui/native-select";
-import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@radix-ui/react-collapsible";
+import { AddBook } from "@/components/ui/add-book-form-custom";
 import {
   Carousel,
   CarouselContent,
@@ -13,80 +9,40 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel"
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
+import { BookCard } from "@/components/ui/book-card-custom";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-4xl flex-col items-center justify-between py-32 px-16 gap-6 bg-white dark:bg-black sm:items-start">
         <h1>Book tracker</h1>
+
+        <Separator />
+
         <div>
           <h2>Profile</h2>
           <Badge variant={"outline"}>username</Badge>
         </div>
 
+        <Separator />
+
         {/* Add new book */}
         <AddBook/>
 
-        
+        <Separator />
+
         <h2>Books to read</h2>
         {/* Carousel start */}
         <Carousel className="w-full">
           <CarouselContent>
             <CarouselItem className="basis-1/3">
               {/* Card start */}
-              <Card className="w-full max-w-sm">
-                <CardHeader>
-                  <CardTitle>Book name</CardTitle>
-                  <CardDescription>Book author, book year</CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <img />IMG
-                </CardContent>
-                <CardFooter className="">
-                  <CardAction className="">
-                    <NativeSelect>
-                      <NativeSelectOption value="">Status</NativeSelectOption>
-                      <NativeSelectOption value="in-progress">In progress</NativeSelectOption>
-                      <NativeSelectOption value="completed">Completed</NativeSelectOption>
-                      <NativeSelectOption value="not-started">Not started</NativeSelectOption>
-                    </NativeSelect>
-                    <Button>Delete book</Button>
-                  </CardAction>
-                </CardFooter>
-              </Card>
+              <BookCard title={"Book name"}/>
             </CarouselItem>
             <CarouselItem className="basis-1/3">
               {/* Card start */}
-              <Card className="w-full max-w-sm">
-                <CardHeader>
-                  <CardTitle>Book name</CardTitle>
-                  <CardDescription>Book author, book year</CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <img />IMG
-                </CardContent>
-                <CardFooter className="">
-                  <CardAction className="">
-                    <NativeSelect>
-                      <NativeSelectOption value="">Status</NativeSelectOption>
-                      <NativeSelectOption value="in-progress">In progress</NativeSelectOption>
-                      <NativeSelectOption value="completed">Completed</NativeSelectOption>
-                      <NativeSelectOption value="not-started">Not started</NativeSelectOption>
-                    </NativeSelect>
-                    <Button>Delete book</Button>
-                  </CardAction>
-                </CardFooter>
-              </Card>
+              <BookCard title={"Book name"}/>
             </CarouselItem>
           </CarouselContent>
 
@@ -94,134 +50,37 @@ export default function Home() {
           <CarouselNext />
         </Carousel>
 
+        <Separator />
+        
         <h2>Reading Books</h2>
         {/* Carousel start */}
         <Carousel className="w-full">
           <CarouselContent>
             <CarouselItem className="basis-1/3">
               {/* Card start */}
-              <Card className="w-full max-w-sm">
-                <CardHeader>
-                  <CardTitle>Book name</CardTitle>
-                  <CardDescription>Book author, book year</CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <img />IMG
-                </CardContent>
-                <CardFooter className="">
-                  <CardAction className="">
-                    Progress<Progress color={"black"} value={80} className="w-full" />
-                    <Collapsible>
-                      <CollapsibleTrigger>Reading dates for this book (Click icon)</CollapsibleTrigger>
-                      <CollapsibleContent>
-                        <Badge variant={"outline"}>01/01/1990</Badge>
-                        <Badge variant={"outline"}>01/01/1990</Badge>
-                        <Badge variant={"outline"}>01/01/1990</Badge>
-                        <Badge variant={"outline"}>01/01/1990</Badge>
-                        <Badge variant={"outline"}>01/01/1990</Badge>
-                      </CollapsibleContent>
-                    </Collapsible>
-                    <Button>Add date</Button>
-
-                    <NativeSelect>
-                      <NativeSelectOption value="">Status</NativeSelectOption>
-                      <NativeSelectOption value="in-progress">In progress</NativeSelectOption>
-                      <NativeSelectOption value="completed">Completed</NativeSelectOption>
-                      <NativeSelectOption value="not-started">Not started</NativeSelectOption>
-                    </NativeSelect>
-                    <Button>Delete book</Button>
-                  </CardAction>
-                </CardFooter>
-              </Card>
+              <BookCard title={"Book name"} enhanced={true}/>
             </CarouselItem>
             <CarouselItem className="basis-1/3">
               {/* Card start */}
-              <Card className="w-full max-w-sm">
-                <CardHeader>
-                  <CardTitle>Book name</CardTitle>
-                  <CardDescription>Book author, book year</CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <img />IMG
-                </CardContent>
-                <CardFooter className="">
-                  <CardAction className="">
-                    Progress<Progress color={"black"} value={80} className="w-full" />
-                    <Collapsible>
-                      <CollapsibleTrigger>Reading dates for this book (Click icon)</CollapsibleTrigger>
-                      <CollapsibleContent>
-                        <Badge variant={"outline"}>01/01/1990</Badge>
-                        <Badge variant={"outline"}>01/01/1990</Badge>
-                        <Badge variant={"outline"}>01/01/1990</Badge>
-                        <Badge variant={"outline"}>01/01/1990</Badge>
-                        <Badge variant={"outline"}>01/01/1990</Badge>
-                      </CollapsibleContent>
-                    </Collapsible>
-                    <Button>Add date</Button>
-
-                    <NativeSelect>
-                      <NativeSelectOption value="">Status</NativeSelectOption>
-                      <NativeSelectOption value="in-progress">In progress</NativeSelectOption>
-                      <NativeSelectOption value="completed">Completed</NativeSelectOption>
-                      <NativeSelectOption value="not-started">Not started</NativeSelectOption>
-                    </NativeSelect>
-                    <Button>Delete book</Button>
-                  </CardAction>
-                </CardFooter>
-              </Card>
+              <BookCard title={"Book name"} enhanced={true}/>
             </CarouselItem>
           </CarouselContent>
           <CarouselPrevious />
           <CarouselNext />
         </Carousel>
+
+        <Separator />
+        
         <h2>Read books</h2>
         {/* Carousel start */}
         <Carousel className="w-full">
           <CarouselContent>
             <CarouselItem className="basis-1/3">
               {/* Card start */}
-              <Card className="w-full max-w-sm">
-                <CardHeader>
-                  <CardTitle>Book name</CardTitle>
-                  <CardDescription>Book author, book year</CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <img />IMG
-                </CardContent>
-                <CardFooter className="">
-                  <CardAction className="">
-                    <NativeSelect>
-                      <NativeSelectOption value="">Status</NativeSelectOption>
-                      <NativeSelectOption value="in-progress">In progress</NativeSelectOption>
-                      <NativeSelectOption value="completed">Completed</NativeSelectOption>
-                      <NativeSelectOption value="not-started">Not started</NativeSelectOption>
-                    </NativeSelect>
-                    <Button>Delete book</Button>
-                  </CardAction>
-                </CardFooter>
-              </Card>
+              <BookCard title={"Book name"}/>
             </CarouselItem>
             <CarouselItem className="basis-1/3">
-              <Card className="w-full max-w-sm">
-                <CardHeader>
-                  <CardTitle>Book name</CardTitle>
-                  <CardDescription>Book author, book year</CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <img />IMG
-                </CardContent>
-                <CardFooter className="">
-                  <CardAction className="">
-                    <NativeSelect>
-                      <NativeSelectOption value="">Status</NativeSelectOption>
-                      <NativeSelectOption value="in-progress">In progress</NativeSelectOption>
-                      <NativeSelectOption value="completed">Completed</NativeSelectOption>
-                      <NativeSelectOption value="not-started">Not started</NativeSelectOption>
-                    </NativeSelect>
-                    <Button>Delete book</Button>
-                  </CardAction>
-                </CardFooter>
-              </Card>
+              <BookCard title={"Book name"}/>
             </CarouselItem>
           </CarouselContent>
           <CarouselNext />
