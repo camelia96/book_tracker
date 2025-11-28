2c4cfcf56dadc6864cbc5c7804d592114082364c
camelia <camelia.96str@gmail.com>
2025-11-28 13:11:43 +0100

    create callback function

--- types/types.ts
+++ types/types.ts
@@ -2,3 +2,6 @@ import { books_profilesModel, booksModel } from "@/generated/prisma/models";
 
 export type BookWithProfiles = booksModel & { books_profiles: {status_id:number}[]; };
 
+
+// Functions
+export type CallbackFunction = (result: number) => void;
