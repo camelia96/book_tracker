646fecf46baa438f19f9a8477c3b908e194ba8b7
camelia <camelia.96str@gmail.com>
2025-11-28 13:07:59 +0100

    create profiles server action

---
+++ actions/profiles.ts
@@ -0,0 +1,8 @@
+"use server";
+
+import { prisma } from "../lib/prisma";
+
+export async function getProfiles() {
+  // Fetch all profiles from database
+  return await prisma.profiles.findMany();
+}
