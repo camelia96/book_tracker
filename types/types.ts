d333b74c84126dd32e3a44584e5438a4395d3f53
camelia <camelia.96str@gmail.com>
2025-11-28 13:41:21 +0100

    add row id to type

--- types/types.ts
+++ types/types.ts
@@ -1,7 +1,8 @@
 import { books_profilesModel, booksModel } from "@/generated/prisma/models";
 
-export type BookWithProfiles = booksModel & { books_profiles: {status_id:number}[]; };
-
+export type BookWithProfiles = booksModel & {
+  books_profiles: { id: number; status_id: number }[];
+};
 
 // Functions
 export type CallbackFunction = (result: number) => void;
