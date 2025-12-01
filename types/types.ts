944c5723fce0bd47218023e776e76d3b69a5ab83
camelia <camelia.96str@gmail.com>
2025-12-01 10:45:26 +0100

    moved .ts files

--- types/types.ts
+++
@@ -1,8 +0,0 @@
-import { books_profilesModel, booksModel } from "@/generated/prisma/models";
-
-export type BookWithProfiles = booksModel & {
-  books_profiles: { id: number; status_id: number }[];
-};
-
-// Functions
-export type CallbackFunction = (result: number) => void;
