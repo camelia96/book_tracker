748232d169c33c8c0c611f05a948c787f2bbeddd
camelia <camelia.96str@gmail.com>
2025-12-01 10:43:19 +0100

    pass prop for update status callback function

--- components/ui/carousel-custom.tsx
+++ components/ui/carousel-custom.tsx
@@ -6,21 +6,23 @@ import {
     CarouselPrevious,
 } from "@/components/ui/carousel"
 import { BookCard } from "./book-card-custom";
-import { BookWithProfiles } from "@/types/types";
+import { BookWithProfiles, UpdateStatusCallbackFunction } from "@/app/types/types";
 
 interface CarouselCustomProps {
     books: BookWithProfiles[];
-    enhanced?: boolean
+    enhanced?: boolean,
+    onBookStatusChange: UpdateStatusCallbackFunction
 }
 
-export function CarouselCustom( {books, enhanced = false} : CarouselCustomProps) {
+export function CarouselCustom({ books, enhanced = false, onBookStatusChange }: CarouselCustomProps) {
 
+    
     {/* Carousel start */ }
     return (<Carousel className="w-full">
         <CarouselContent>
             {books.map((e) =>
             (<CarouselItem key={e.id} className="basis-1/3">
-                <BookCard book={e} enhanced={enhanced} />
+                <BookCard book={e} enhanced={enhanced} onStatusChange={onBookStatusChange} />
             </CarouselItem>))}
 
         </CarouselContent>
