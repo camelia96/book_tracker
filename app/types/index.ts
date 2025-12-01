2f995b212ac332fc9505fe7e7cee8822b6ece4ee
camelia <camelia.96str@gmail.com>
2025-12-01 17:52:00 +0100

    types change filename

---
+++ app/types/index.ts
@@ -0,0 +1,34 @@
+import {
+  books_profiles_progressModel,
+  books_profilesModel,
+  booksModel,
+} from "@/generated/prisma/models";
+
+export type BookWithProfiles = booksModel & {
+  books_profiles: { id: number; status_id: number }[];
+};
+
+// Callback Functions
+export type AddBookCallbackFunction = (newBookId: number) => void;
+
+export type UpdateStatusCallbackFunction = (book: BookWithProfiles) => void;
+
+export type AddReadingDateCallbackFunction = (
+  readingDate: books_profiles_progressModel
+) => void;
+
+export type DeleteBookCallbackFunction = (book: booksModel) => void;
+
+export interface SingleBookProps {
+  book: BookWithProfiles;
+  enhanced?: boolean;
+  onStatusChange: UpdateStatusCallbackFunction;
+  onDeleteBook: DeleteBookCallbackFunction;
+}
+
+export interface ListBookProps {
+  books: BookWithProfiles[];
+  enhanced?: boolean;
+  onStatusChange: UpdateStatusCallbackFunction;
+  onDeleteBook: DeleteBookCallbackFunction;
+}
