10350473890822309dbde7e9033d043e66ef375f
camelia <camelia.96str@gmail.com>
2025-12-02 10:58:22 +0100

    add trycatch create/update/delete functions

--- actions/books_profiles.ts
+++ actions/books_profiles.ts
@@ -5,32 +5,44 @@ import { prisma } from "../lib/prisma";
 
 // Create
 export async function createBookProfile(userId: number, bookId: number) {
-  return await prisma.books_profiles.create({
-    data: {
-      book_id: bookId,
-      profile_id: userId,
-      // By default we start with book not read
-      status_id: STATUSES_IDS.not_started,
-    },
-  });
+  try {
+    return await prisma.books_profiles.create({
+      data: {
+        book_id: bookId,
+        profile_id: userId,
+        // By default we start with book not read
+        status_id: STATUSES_IDS.not_started,
+      },
+    });
+  } catch (error) {
+    return undefined;
+  }
 }
 
 // Read
 
 // Update
 export async function updateBookStatus(rowId: number, newStatusId: number) {
-  return await prisma.books_profiles.update({
-    data: {
-      status_id: newStatusId,
-    },
-    where: { id: rowId },
-    select: {
-      book_id: true,
-    },
-  });
+  try {
+    return await prisma.books_profiles.update({
+      data: {
+        status_id: newStatusId,
+      },
+      where: { id: rowId },
+      select: {
+        book_id: true,
+      },
+    });
+  } catch (error) {
+    return undefined;
+  }
 }
 
 // Delete
 export async function deleteBookProfile(id: number) {
-  return await prisma.books_profiles.delete({ where: { id: id } });
+  try {
+    return await prisma.books_profiles.delete({ where: { id: id } });
+  } catch (error) {
+    return undefined;
+  }
 }
