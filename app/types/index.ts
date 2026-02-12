8c20a6299873dfc7ad9266b4af58d0606c767b55
camelia <camelia.96str@gmail.com>
2026-02-12 15:50:38 +0100

    delete callback types + change BookWithProfiles types to work for all components

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,33 +1,8 @@
-import {
-  books_profiles_progressModel,
-  booksModel,
-} from "@/generated/prisma/models";
+import { books_profilesGetPayload } from "@/generated/prisma/models";
 
-export type BookWithProfiles = booksModel & {
-  books_profiles: { id: number; status_id: number }[];
-};
-
-// Callback Functions
-export type AddBookCallbackFunction = (newBook: BookWithProfiles) => void;
-
-export type UpdateStatusCallbackFunction = (book: BookWithProfiles) => void;
-
-export type AddReadingDateCallbackFunction = (
-  readingDate: books_profiles_progressModel
-) => void;
-
-export type DeleteBookCallbackFunction = (book: booksModel) => void;
-
-export interface SingleBookProps {
-  book: BookWithProfiles;
-  enhanced?: boolean;
-  onStatusChange: UpdateStatusCallbackFunction;
-  onDeleteBook: DeleteBookCallbackFunction;
-}
-
-export interface ListBookProps {
-  books: BookWithProfiles[];
-  enhanced?: boolean;
-  onStatusChange: UpdateStatusCallbackFunction;
-  onDeleteBook: DeleteBookCallbackFunction;
-}
+export type BookType = books_profilesGetPayload<{
+  include: {
+    books: { include: { categories: true } };
+    books_profiles_progress: true;
+  };
+}>;
